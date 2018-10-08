986fbc43cec96cf1c0f8a1696c45747f3fba80b4
Rene Ome <ing.rene.ome@gmail.com>
2018-10-08 09:53:47 -0500

    se ajusta presentaciones Experian

---
+++ Stefanini/Stefanini.Clientes/Stefanini.Cliente.Experian/mapa Impedimentos Infraestructura.mm
@@ -0,0 +1,11 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1538940087580" ID="ID_1625415704" MODIFIED="1538940113767" TEXT="Infraestructura">
+<node CREATED="1538940114517" ID="ID_444103581" MODIFIED="1538940306447" POSITION="right" TEXT="Alta demanda de solicitudes"/>
+<node CREATED="1538940134201" ID="ID_128520078" MODIFIED="1538940150447" POSITION="right" TEXT="falta de comunicacion con otros equipos"/>
+<node CREATED="1538940151618" ID="ID_1569577186" MODIFIED="1538940163935" POSITION="right" TEXT="tiempos de espera muy altos"/>
+<node CREATED="1538940164777" ID="ID_869224297" MODIFIED="1538940175238" POSITION="right" TEXT="burocracia en los procesos"/>
+<node CREATED="1538940177249" ID="ID_795198097" MODIFIED="1538940177249" POSITION="right" TEXT=""/>
+<node CREATED="1538940218575" ID="ID_833668841" MODIFIED="1538940218575" POSITION="left" TEXT=""/>
+</node>
+</map>
